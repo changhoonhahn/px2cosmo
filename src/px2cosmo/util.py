2641a77f216237e831dbdd76dc52e03d1154772c
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2026-06-05 16:16:01 +0200

    function for reading the best model

--- src/px2cosmo/util.py
+++ src/px2cosmo/util.py
@@ -1,7 +1,19 @@
 '''
 useful functions
 '''
+import os
+import torch
+import optuna
 
-def LF(Muv, z, phi): 
+
+def best_model(study_name, output_dir):
+    storage = 'sqlite:///%s/%s/%s.db' % (output_dir, study_name, study_name)
+    study   = optuna.load_study(study_name=study_name, storage=storage)
+    best    = study.best_trial
+    fmodel  = os.path.join(output_dir, study_name, '%s.%i.pt' % (study_name, best.number))
+    return torch.load(fmodel), best
+
+
+def LF(Muv, z, phi):
     alpha, beta, gamma, Muv_s = phi
     return (10**(gamma * (z - 9.)))/(10**(0.4*(alpha + 1)*(Muv - Muv_s) + 10**(0.4*(beta + 1)*(Muv - Muv_s))))
