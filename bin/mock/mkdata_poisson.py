007c429a0fc1afa8006ecbdecaeee616ad52f19b
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2026-07-07 12:33:42 +0200

    multiprocessing to speed up mkdata_poisson.py

--- bin/mock/mkdata_poisson.py
+++ bin/mock/mkdata_poisson.py
@@ -1,45 +1,48 @@
 '''
 
-make training data with homoskedastic noise 
+make training data for the Poisson Likelihood NDE 
 
 '''
 import os, sys
-import numpy as np 
-from tqdm import tqdm 
-from px2cosmo import fm as FM 
-from px2cosmo import util as UT 
+import numpy as np
+from multiprocessing import Pool
+from tqdm import tqdm
 
-Nmocks  = int(sys.argv[1]) 
-outdir  = sys.argv[2]
+from px2cosmo import fm as FM
+from px2cosmo import util as UT
 
-bounds = UT._prior_range_default()
 
-# sample LF parameters 
-# omega_true = np.array([-1.65, -1.5, -0.2, -19.5]) # fit by eye to zeus21 output
-# phi_amp = 6e-3 
-omegas = np.array([
-    np.random.uniform(bounds[0][0], bounds[0][1], size=Nmocks), 
-    np.random.uniform(bounds[1][0], bounds[1][1], size=Nmocks),
-    np.random.uniform(bounds[2][0], bounds[2][1], size=Nmocks), 
-    np.random.uniform(bounds[3][0], bounds[3][1], size=Nmocks), 
-    np.random.uniform(bounds[4][0], bounds[4][1], size=Nmocks)
-]).T
+def _worker(omega):
+    mock0 = FM.forwardmodel(omega[:-1], name='z14', phi_amp=omega[-1])
+    mock1 = FM.forwardmodel(omega[:-1], name='z11', phi_amp=omega[-1])
+    mock2 = FM.forwardmodel(omega[:-1], name='z9',  phi_amp=omega[-1])
+    return omega.tolist() + [mock0.shape[0], mock1.shape[0], mock2.shape[0]]
 
-fdata = os.path.join(outdir, 'mock_N%i_poisson.v1.dat' % Nmocks)
-with open(fdata, "w") as f:
-    f.write("# alpha, beta, gamma, Mnuvs, phi_amp, N(z=14), N(z=11), N(z=9)\n")
 
-for i in tqdm(range(Nmocks)): 
-    # forward model 
-    mock0 = FM.forwardmodel(omegas[i,:-1], name='z14', phi_amp=omegas[i,-1])
-    mock1 = FM.forwardmodel(omegas[i,:-1], name='z11', phi_amp=omegas[i,-1])
-    mock2 = FM.forwardmodel(omegas[i,:-1], name='z9', phi_amp=omegas[i,-1])
+if __name__ == '__main__':
+    Nmocks  = int(sys.argv[1])
+    outdir  = sys.argv[2]
 
-    with open(fdata, "a") as f:
-        f.write(" ".join(map(str, omegas[i])) + " " +  
-                " ".join(map(str, [mock0.shape[0], mock1.shape[0], mock2.shape[0]])) + "\n")
+    bounds = UT._prior_range_default()
 
-# save to numpy for faster I/O
-data = np.loadtxt(fdata, skiprows=1) 
-np.save(fdata.replace('.dat', '.npy'), data) 
+    omegas = np.array([
+        np.random.uniform(bounds[0][0], bounds[0][1], size=Nmocks),
+        np.random.uniform(bounds[1][0], bounds[1][1], size=Nmocks),
+        np.random.uniform(bounds[2][0], bounds[2][1], size=Nmocks),
+        np.random.uniform(bounds[3][0], bounds[3][1], size=Nmocks),
+        np.random.uniform(bounds[4][0], bounds[4][1], size=Nmocks)
+    ]).T
+
+    with Pool() as pool:
+        rows = list(tqdm(pool.imap(_worker, omegas), total=Nmocks))
+
+    data = np.array(rows)
+
+    fdata = os.path.join(outdir, 'mock_N%i_poisson.v1.dat' % Nmocks)
+    with open(fdata, "w") as f:
+        f.write("# alpha, beta, gamma, Mnuvs, phi_amp, N(z=14), N(z=11), N(z=9)\n")
+        for row in rows:
+            f.write(" ".join(map(str, row)) + "\n")
+
+    np.save(fdata.replace('.dat', '.npy'), data)
 
