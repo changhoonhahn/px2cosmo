31b3fe3532979776d297c9308b7efc9850d1e412
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2026-07-07 11:32:30 +0200

    updated uncertainty range to be more consistent with CEERS catalog in Finkelstein et al.

--- src/px2cosmo/fm.py
+++ src/px2cosmo/fm.py
@@ -86,10 +86,10 @@ def forwardmodel(phi, name='test0', phi_amp=6e-3):
 
         # apply simple Gaussian noise model with noise that sufficiently 
         # encompass noise levels of the CEERS sample 
-        sig_photoz = np.random.uniform(0.05, 1, size=mock.shape[0])
+        sig_photoz = np.random.uniform(0.03, 1, size=mock.shape[0])
         mock_photoz = mock[:,0] + sig_photoz * np.random.normal(size=mock.shape[0])
         
-        sig_Muv = np.random.uniform(0.05, 0.5, size=mock.shape[0])
+        sig_Muv = np.random.uniform(0.01, 0.5, size=mock.shape[0])
         mock_Muv = mock[:,1] + sig_Muv * np.random.normal(size=mock.shape[0])
 
         return np.vstack([mock_photoz, mock_Muv, sig_photoz, sig_Muv]).T[select]
@@ -166,28 +166,6 @@ def selection_function(mock, name='mock0'):
         prob_z = np.exp(-(mock[:,0] - 14.0)**2)
         select_z = prob_z > np.random.uniform(size=mock.shape[0])
 
-        # convert mock absolute magnitude to apparent magnitudes
-        dl = dl_spline(mock[:,0])
-        mock_muv = mock[:,1] + 5 * np.log10(dl / 10.) 
-
-        # m_uv selection weights
-        w_muv_select = _select_muv(mock_muv, c_erf0=1., c_erf1=32)
-        select_muv = np.random.uniform(size=mock.shape[0]) < w_muv_select
-
-        return select_z & select_muv
-
-    elif name == 'multiple': 
-        prob_z0 = np.exp(-(mock[:,0] - 7.0)**2)
-        select_z0 = prob_z0 > np.random.uniform(size=mock.shape[0])
-
-        prob_z1 = np.exp(-(mock[:,0] - 9.0)**2)
-        select_z1 = (prob_z1 > np.random.uniform(size=mock.shape[0])) & (~select_z0)
-
-        prob_z2 = np.exp(-(mock[:,0] - 11.0)**2)
-        select_z2 = (prob_z2 > np.random.uniform(size=mock.shape[0])) & (~select_z0) & (~select_z1)
-
-        select_z = select_z0 | select_z1 | select_z2
-        
         # convert mock absolute magnitude to apparent magnitudes
         dl = dl_spline(mock[:,0])
         mock_muv = mock[:,1] + 5 * np.log10(dl / 10.) 
